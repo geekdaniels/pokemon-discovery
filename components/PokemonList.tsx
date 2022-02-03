004f28d37378eda4810cd5ddc214befaaf0c48ea
Olufemi Daniel <olufemidaniels07@gmail.com>
2022-02-03 01:10:42 +0100

    Clean up pokemon list

--- components/PokemonList.tsx
+++ components/PokemonList.tsx
@@ -1,11 +1,20 @@
-import type { Pokemon } from "../TYPES";
+import Link from "next/link";
+import type { Pokemon } from "../types";
 
 export const PokemonList = ({ list }: { list: Pokemon[] }) => {
   return (
-    <ul>
-      {list.map((pokemon, index) => (
-        <li key={index}>{pokemon.name}</li>
-      ))}
-    </ul>
+    <div className="bg-white shadow rounded">
+      <ul className="divide-y divide-gray-200">
+        {list.map((pokemon, index) => (
+          <li key={index}>
+            <Link href={`/pokemon/${pokemon.name}`}>
+              <a href="" className="block hover:bg-gray-100">
+                <div className="p-4 capitalize">{pokemon.name}</div>
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
